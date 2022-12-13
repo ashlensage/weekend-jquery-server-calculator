01000566b2dd8e673cbdc686f616908af2ab9be7
Ashlen Goff <ashlensage@icloud.com>
2022-12-13 14:29:13 -0600

    Server Side Calculator

--- server/public/scripts/script.js
+++ server/public/scripts/script.js
@@ -5,10 +5,33 @@ function handleReady() {
     $('.calculator').submit(function(event) {
         event.preventDefault();
         handleSubmit(event);
-    })
+    });
     $('.operator').on('click', 'input', function(event) {
         setOperator(event);
-    })
+    });
+    getHistory();
+};
+
+function getHistory() {
+    $.ajax({
+        type: "GET",
+        url: '/history',
+        success: function(historyRes) {
+            console.log('historyRes', historyRes);
+            renderHistory(historyRes);
+        },
+      });
+};
+
+function renderHistory({history}) {
+    const historyItems = [];
+    $(history).each(function() {
+        historyItems.push(`<li>
+            ${this.firstNumber} ${this.operator} ${this.secondNumber} = ${this.result}
+        </li>`);
+    });
+    console.log('historyItems', historyItems);
+    $('.history').empty().append(historyItems);
 };
 
 function setOperator({target}) {
@@ -23,8 +46,19 @@ function handleSubmit({target}) {
     console.log('handleSubmit, numbers:', numbers);
     $(numbers).each(function(){
         console.log('handleSubmit, each this:', this);
-        payLoad[this.name] = $(this).val();
+        payLoad[this.name] = parseInt( $(this).val() );
     })
     console.log('handleSubmit, payLoad:', payLoad);
+    $.ajax({
+        type: "POST",
+        url: '/calculate',
+        data: payLoad,
+        success: function(response) {
+            console.log('response', response);
+            $('.result').text(response.result);
+            $('.calculator').get(0).reset();
+            getHistory();
+        },
+      });
 };
 
--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser')
 const app = express();
 const PORT = 5000;
+const history = [];
 
 // This must be added before GET & POST routes.
 app.use(bodyParser.urlencoded({extended:true}))
@@ -9,80 +10,51 @@ app.use(bodyParser.urlencoded({extended:true}))
 // Serve up static files (HTML, CSS, Client JS)
 app.use(express.static('server/public'));
 
-
-function randomNumber(min, max){
-  return Math.floor(Math.random() * (1 + max - min) + min);
-}
-
-// number need to be guessed
-let random = randomNumber(1, 25)
-
-// comparison function
-
-
 // GET & POST Routes go here
-app.post('/', (req, res)=>{
-  console.log(req.body);
-  let arr = [];
-
-  if(req.body.guess1 == random){
-    console.log('correct');
-    arr.push({
-      update:"correct",
-      guesser: 'guesser1'
-    })
-  } else if (req.body.guess2 == random) {
-    console.log('correct');
-    arr.push({
-      update:"correct",
-      guesser: 'guesser2'
-    })
-
-  } else {
-    console.log('incorrect')
-    if(req.body.guess1 > random) {
-      console.log('your guess is greater')
-      arr.push({
-        update:"higher",
-        guesser: 'guesser1'
-      })
-    } else {
-      console.log('your guess is lower')
-      arr.push({
-        update:"lower",
-        guesser: 'guesser1'
-      })
-    }
-
-    if(req.body.guess2 > random) {
-      console.log('your guess is greater')
-      arr.push({
-        update:"higher",
-        guesser: 'guesser2'
-      })
-    }
-    else{
-      console.log('your guess is lower');
-      arr.push({
-        update:"lower",
-        guesser: 'guesser2'
-      })
-    }
-
-  }
-
-  app.post('/generate', (req, res)=>{
-    random = req.body.num;
-    console.log(random);
-  })
-  console.log(random);
-  console.log(arr);
-  // ['guess1' ,'guess2']
-
-  res.send(arr);
+app.get('/history', (req, res)=>{
+  res.send({history});
 });
 
+app.post('/calculate', (req, res)=>{
+  const {
+    operator,
+    firstNumber,
+    secondNumber
+  } = req.body;
+  const num1 = parseInt(firstNumber);
+  const num2 = parseInt(secondNumber);
+  let result = 0;
+  let operatorSymbol = '';
+  console.log(req.body);
+  switch (operator) {
+    case 'add':
+      result = num1 + num2;
+      operatorSymbol = '+';
+      break;
+    case 'subtract':
+      result = num1 - num2;
+      operatorSymbol = '&minus;';
+      break;
+    case 'multiply':
+      result = num1 * num2;
+      operatorSymbol = '&times;'
+      break;
+    case 'divide':
+      result = num1 / num2;
+      operatorSymbol = '&divide;';
+      break;
+    default:
+      console.log('opperator error');
+  }  
+  history.push({
+    operator: operatorSymbol,
+    firstNumber: num1,
+    secondNumber: num2,
+    result
+  });
+  res.send({result});
+});
 
 app.listen(PORT, () => {
   console.log ('Server is running on port', PORT)
-})
+});
